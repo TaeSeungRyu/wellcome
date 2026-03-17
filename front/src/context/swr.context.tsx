e11b0a08adcf0b8007c606b06b25b8f40e43d016
ts.Ryu <lts06069@naver.com>
2026-03-17 16:12:30 +0900

    프론트 작업 - 62
     1. swr 연습 계속!

--- front/src/context/swr.context.tsx
+++ front/src/context/swr.context.tsx
@@ -1,7 +1,65 @@
-import { SWRConfig } from "swr";
+import { useCallback, useEffect, useRef } from "react";
+import { SWRConfig, type Middleware, type SWRHook } from "swr";
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+// 키가 변경되더라도 데이터를 유지하기 위한 SWR 미들웨어입니다.
+const laggy: Middleware = (useSWRNext: SWRHook) => {
+  return (key, fetcher, config) => {
+    // 이전에 반환된 데이터를 저장하기 위해 ref를 사용합니다.
+    const laggyDataRef = useRef<undefined | any>(undefined);
+    // 실제 SWR hook.
+    const swr = useSWRNext(key, fetcher, config);
+    useEffect(() => {
+      // 데이터가 undefined가 아니면 ref를 업데이트합니다.
+      if (swr.data !== undefined) {
+        laggyDataRef.current = swr.data;
+      }
+    }, [swr.data]);
+    // 지연 데이터가 존재할 경우 이를 제거하기 위한 메서드를 노출합니다.
+    const resetLaggy = useCallback(() => {
+      laggyDataRef.current = undefined;
+    }, []);
+    // 현재 데이터가 undefined인 경우에 이전 데이터로 폴백
+    const dataOrLaggyData =
+      swr.data === undefined ? laggyDataRef.current : swr.data;
+    // 이전 데이터를 보여주고 있나요?
+    const isLagging =
+      swr.data === undefined && laggyDataRef.current !== undefined;
+    // `isLagging` 필드 또한 SWR에 추가합니다.
+    return Object.assign({}, swr, {
+      data: dataOrLaggyData,
+      isLagging,
+      resetLaggy,
+    });
+  };
+};
+
+// SWR 미들웨어: 로깅 기능 추가
+const logger: Middleware = (useSWRNext: SWRHook) => {
+  return (key, fetcher, config) => {
+    const swr = useSWRNext(key, fetcher, config);
+    useEffect(() => {
+      console.log("SWR 인스턴스 실행:", key);
+    }, [key]);
+    useEffect(() => {
+      if (swr.data) console.log("데이터 수신:", key, swr.data);
+      if (swr.error) console.error("에러 발생:", key, swr.error);
+    }, [swr.data, swr.error, key]);
+    return swr;
+  };
+};
 
 export function SWRProviders({ children }: any) {
-  return <SWRConfig value={{ fetcher }}>{children}</SWRConfig>;
+  return (
+    <SWRConfig
+      value={{
+        onError: (error, key) => {
+          //에러를 전역으로 처리 가능
+          console.error(`Error fetching data for key: ${key}`, error);
+        },
+        use: [logger, laggy],
+      }}
+    >
+      {children}
+    </SWRConfig>
+  );
 }
--- front/src/routes/home/dashboard-swr/route.tsx
+++ front/src/routes/home/dashboard-swr/route.tsx
@@ -1,16 +1,86 @@
 import { createFileRoute } from "@tanstack/react-router";
 import useSWR from "swr";
-import { requestBoardList } from "../dashboard/-/board.repository";
+import useSWRMutation from "swr/mutation";
+
+import {
+  requestBoardInsert,
+  requestBoardList,
+} from "../dashboard/-/board.repository";
+import { useEffect, useState } from "react";
 
 export const Route = createFileRoute("/home/dashboard-swr")({
   component: RouteComponent,
 });
 
+const getSampleData = async () => {
+  const title = Math.random().toString(36).substring(2, 7); // 랜덤 제목 생성
+  const contents = Math.random().toString(36).substring(2, 15); // 랜덤 내용 생성
+  return { title, contents };
+};
+
 function RouteComponent() {
-  const page = 1;
+  const [page, setPage] = useState(1);
   const limit = 5;
-  const { data } = useSWR(["boardList", page, limit], ([, page, limit]) =>
-    requestBoardList(page, limit),
+  const { data, mutate: refetchList } = useSWR(
+    ["boardList", page, limit],
+    ([, page, limit]) => requestBoardList(page, limit),
   );
-  return <div>Hello "/home/dashboard-swr"!</div>;
+
+  const { trigger, isMutating, error } = useSWRMutation(
+    ["boardList", page, limit],
+    async () => {
+      const { title, contents } = await getSampleData();
+      await requestBoardInsert(title, contents); // 서버에 데이터 추가 요청
+    },
+    {
+      onSuccess: (data) => {
+        console.log("성공!", data); //추가 성공 후 별도의 refetchList 호출 없이 SWR이 자동으로 캐시를 갱신
+      },
+      onError: (err) => {
+        console.error("실패!", err);
+      },
+    },
+  );
+
+  //게시글 추가 함수
+  const handleAddBoard = async () => {
+    const { title, contents } = await getSampleData();
+    try {
+      // 서버에 데이터 추가 요청 (SWR과 별개인 일반 비동기 함수)
+      await requestBoardInsert(title, contents);
+      //추가 성공 후 'boardList' 캐시를 갱신 (서버에서 다시 불러오기)
+      refetchList();
+      alert("추가되었습니다!");
+    } catch (e) {
+      console.error("추가 실패", e);
+    }
+  };
+  useEffect(() => {
+    if (data) console.log("데이터 수신:", data);
+  }, [data]);
+  return (
+    <div>
+      <h1> 목록</h1>
+      <button onClick={handleAddBoard}>
+        {isMutating ? "저장 중..." : "추가(refetchList 방법)"}
+      </button>
+      <button
+        onClick={() => {
+          trigger();
+        }}
+        disabled={isMutating}
+      >
+        추가(useSWRMutation 사용)
+      </button>
+      {error && <p>추가 실패: {error.message}</p>}
+      <ul>
+        {data?.map((board: any) => (
+          <li key={board._id}>
+            <h2>{board.title}</h2>
+            <p>{board.contents}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
