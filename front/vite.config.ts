a6a525b980faf887434a9ced5d635f61f71f28ed
ts.Ryu <lts06069@naver.com>
2026-04-08 09:14:41 +0900

    프론트 작업 - 72
    1. 스토리북 연습

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,38 +1,60 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
 import viteReact from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import { fileURLToPath, URL } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
+import path from 'node:path';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 const proxy = {
   "/api": {
     target: "http://localhost:8080",
     changeOrigin: true,
-    rewrite: (path: string) => path.replace(/^\/api/, ""),
-  },
+    rewrite: (path: string) => path.replace(/^\/api/, "")
+  }
 };
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    devtools(),
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-    }),
-    viteReact(),
-    tailwindcss(),
-  ],
+  plugins: [devtools(), tanstackRouter({
+    target: "react",
+    autoCodeSplitting: true
+  }), viteReact(), tailwindcss()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
   },
   // API 서버 프록시 설정
   server: {
     host: "0.0.0.0",
     cors: false,
-    proxy,
+    proxy
   },
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: 'playwright',
+          instances: [{
+            browser: 'chromium'
+          }]
+        }
+      }
+    }]
+  }
 });
